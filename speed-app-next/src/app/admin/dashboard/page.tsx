9d3f5803847d67714b650005820e253ff2349a91
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2024-10-06 17:58:13 +1300

    Dashboard check if logged in
    Will automatically navigate off the dashboard if the state of logged in changes

--- speed-app-next/src/app/admin/dashboard/page.tsx
+++ speed-app-next/src/app/admin/dashboard/page.tsx
@@ -2,21 +2,19 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useAuth } from '@/utils/AuthContext';
 
 const AdminDashboard = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Check if the token exists
-    const token = localStorage.getItem('token');
-
-    // If no token, redirect to login page
-    if (!token) {
-      router.push('/login');
-    }
-
-  }, [router]);
-
+    const { isLoggedIn } = useAuth(); // Access the login state
+    const router = useRouter();
+  
+    // Automatically redirect to login if not logged in
+    useEffect(() => {
+      if (!isLoggedIn) {
+        router.push('/login'); // Redirect to login if the user is logged out
+      }
+    }, [isLoggedIn, router]);
+  
   return (
     <div className="container py-5">
       <h1>Welcome to the Admin Dashboard</h1>
